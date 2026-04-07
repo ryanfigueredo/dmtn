30f5284e5269e37ef69b58212459811ace1292fe
Ryan Figueredo <ryan.figueredo059@gmail.com>
2026-04-07 00:58:37 -0300

    Adiciona rastreamento do Apollo ao layout para monitoramento de usuários

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,24 @@ export default function RootLayout({
           `}
         </Script>
 
+        {/* Apollo Tracking */}
+        <Script id="apollo-tracker" strategy="afterInteractive">
+          {`
+            function initApollo(){
+              var n=Math.random().toString(36).substring(7),
+                  o=document.createElement("script");
+              o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n;
+              o.async=true;
+              o.defer=true;
+              o.onload=function(){
+                window.trackingFunctions.onLoad({appId:"69d41c7ed11bea000d07edbb"});
+              };
+              document.head.appendChild(o);
+            }
+            initApollo();
+          `}
+        </Script>
+
         <ToastProvider>
           {children}
           <ToastViewport />
